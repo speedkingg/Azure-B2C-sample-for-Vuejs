6ef379e5d90c978fefdc68d12639699cab66f188
MiracleHedgehog <panopticon130@gmail.com>
2020-04-17 21:02:35 +0900

    サインインボタンのアイコン修正

--- src/components/SignInButton.vue
+++ src/components/SignInButton.vue
@@ -1,6 +1,6 @@
 <template>
   <v-btn @click="SignIn" v-if="!isSignin" color="primary" class="text-capitalize" outlined>
-    <v-icon left>mdi-windows</v-icon>Sign In
+    <v-icon left>mdi-microsoft</v-icon>Sign In
     <v-icon small right>mdi-checkbox-multiple-blank-outline</v-icon>
   </v-btn>
 </template>
